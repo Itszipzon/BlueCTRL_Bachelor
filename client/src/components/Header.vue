7adaf5a29860c5b52faa3ec00d04945024c2f758
runemola <runemola@stud.ntnu.no>
2025-03-03 14:01:01 +0100

    Initiated header and main page

---
+++ client/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header-container">
+    <div class="topbar"></div>
+    <div class="sidebar"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.topbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 75px;
+  background-color: #114155;
+  z-index: 1001;
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 75px;
+  height: 100vh;
+  background-color: #114155;
+  z-index: 1001;
+}
+</style>
