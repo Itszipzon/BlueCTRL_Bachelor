98488978773718fcfe5a42f883d056abfad06a76
Jan Nordskog <jan.nordskog@gmail.com>
2025-03-13 17:11:52 +0100

    forward api for bluectrl

--- server/src/main/java/no/itszipzon/api/ApiTest.java
+++ server/src/main/java/no/itszipzon/api/ApiTest.java
@@ -1,10 +1,15 @@
 package no.itszipzon.api;
 
+import java.util.Base64;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @RestController
 @RequestMapping("/api")
@@ -13,17 +18,22 @@ public class ApiTest {
     private final WebClient webClient;
 
     public ApiTest(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://api.chucknorris.io").build();
+        Dotenv dotenv = Dotenv.load();
+        String baseUrl = dotenv.get("API_BASE_URL");  //Rune du må legge til API_BASE_URL med url i .env
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
     }
 
-    @GetMapping("/joke")
-    public ResponseEntity<String> getJoke() {
-        String joke = webClient.get()
-                .uri("/jokes/random")
+    @GetMapping("/fuel-types")
+    public ResponseEntity<String> getfueltypes(
+            @RequestHeader("Authorization") String authorizationHeader) {
+        String fueltypes = webClient.get()
+                .uri("/fuel-types")
+                .headers(headers -> headers.set("Authorization",
+                        authorizationHeader))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
 
-        return ResponseEntity.ok(joke);
+        return ResponseEntity.ok(fueltypes);
     }
 }
