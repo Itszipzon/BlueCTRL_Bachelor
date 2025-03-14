35a50aa3d04c9c678b8d2cb86051685ccb0f7551
Eirik <eirik.imrik@gmail.com>
2025-03-14 12:58:20 +0100

    added box for boat data to details page

--- client/src/views/DetailsPage.vue
+++ client/src/views/DetailsPage.vue
@@ -4,38 +4,61 @@
   <div class="page-container">
     <h1 class="header">🇳🇴 Boat name</h1>
     <div class="content-container">
-      <div class="boat-tilt"></div>
-      <div class="boat-slope"></div>
-      <div class="mini-map"></div>
+      <div class="top-container">
+        <div class="boat-tilt"></div>
+        <div class="boat-slope"></div>
+        <div class="mini-map"></div>
+      </div>
+      <div class="middle-container">
+        <div class="data-box"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.page-container {
+}
 .header {
   font-size: x-large;
   color: #000;
 }
-
 .content-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  gap: 20px;
+}
+
+.top-container {
+  display: flex;
+  justify-content: center;
+  gap: 26px;
+}
+.middle-container {
+  display: flex;
+  justify-content: center;
 }
 
 .boat-tilt {
-    background-color: #ffffff;
-    width: 400px;
-    height: 287px;
+  background-color: #ffffff;
+  width: 400px;
+  height: 287px;
 }
 .boat-slope {
-    background-color: #ffffff;
-    width: 400px;
-    height: 287px;
+  background-color: #ffffff;
+  width: 400px;
+  height: 287px;
 }
 .mini-map {
-    background-color: #ffffff;
-    width: 400px;
-    height: 287px;
+  background-color: #ffffff;
+  width: 400px;
+  height: 287px;
+}
+
+.data-box {
+  width: 1251px;
+  height: 287px;
+  background-color: #ffffff;
+  margin-top: 26px;
 }
 </style>
