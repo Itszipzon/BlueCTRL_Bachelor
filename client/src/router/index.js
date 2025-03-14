48e78ab389a6656eca5f7f8e4a7b6565ae2577c1
Eirik <eirik.imrik@gmail.com>
2025-03-14 12:35:32 +0100

    added detailspage route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
+import Details from '../views/DetailsPage.vue'
 
 Vue.use(VueRouter)
 
@@ -13,14 +14,12 @@ const router = new VueRouter({
       name: 'main',
       component: Main
     },
-/*     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    } */
+    {
+      path: '/details',
+      name: 'details',
+      component: Details
+
+    },
   ]
 })
 
--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,10 +1,14 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-
+  <div class="header">
+    <h1>Map page</h1>
+  </div>
 </template>
 
 <style scoped>
-
+.header {
+  font-size: x-large;
+  color: #000;
+}
 </style>
