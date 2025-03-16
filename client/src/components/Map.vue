fda610414a3253280be145e925006da5491a9e45
runemola <runemola@stud.ntnu.no>
2025-03-16 01:39:03 +0100

    Added map function

---
+++ client/src/components/Map.vue
@@ -0,0 +1,70 @@
+<template>
+  <l-map :center="[center.lat, center.lng]" :zoom="13" style="height: 100%; width: 100%;" ref="leafletMap"
+    @ready="onMapReady">
+    <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="Map data © OpenStreetMap contributors" />
+    <l-marker v-for="(marker, index) in markers" :key="index"
+      :lat-lng="[marker.gpsPosition.latitude, marker.gpsPosition.longitude]" :icon="createCustomIcon()"
+      @click="onMarkerClick(marker)">
+    </l-marker>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import MapMarker from '../assets/icons/boatmarker.svg';
+
+export default {
+  props: {
+    large: Boolean,
+    markers: Array,
+    center: Object,
+    resize: Function,
+    setValues: Function,
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+  },
+  methods: {
+    onMapReady() {
+      this.$emit('map-ready', this.$refs.leafletMap.mapObject);
+    },
+
+    createCustomIcon() {
+      return L.divIcon({
+        className: 'custom-icon',
+        html: `<img src="${MapMarker}" alt="Marker Icon" style="width: 40px; height: 40px;" />`,
+        iconSize: [40, 40],
+        iconAnchor: [20, 20],
+        popupAnchor: [0, -20],
+      });
+    },
+
+    onMarkerClick(marker) {
+
+
+      if (this.large) {
+        this.resize();
+        setTimeout(() => {
+          this.setValues(marker);
+        }, 500);
+      } else {
+        this.setValues(marker);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-icon img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+</style>
--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,14 +1,84 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted  } from 'vue';
+import Map from '../components/Map.vue';
+
+const markers = ref([]);
+const selectedMarker = ref({});
+
+const largeMap = ref(true);
+let mapObject = null;
+
+const center = ref({
+  lat: 62.47225,
+  lng: 6.15492,
+});
+
+const resizeStyle = ref(getResizeStyle());
+
+function getResizeStyle() {
+  return window.innerWidth < 1615 && !largeMap.value
+    ? { right: (window.innerWidth / 2) - 75 - (750/2 - 15/2 - 35) + "px" }
+    : {};
+}
+
+function toggleMapSize() {
+  largeMap.value = !largeMap.value;
+
+  handleResize();
+  if (mapObject) {
+    const currentCenter = mapObject.getCenter();
+
+    setTimeout(() => {
+      mapObject.invalidateSize();
+      mapObject.setView(currentCenter, mapObject.getZoom());
+    }, 300);
+  }
+}
+
+function onMapReady(map) {
+  mapObject = map;
+  console.log(mapObject.getBounds());
+}
+
+function setCenter(lat, lng) {
+  center.value = { lat, lng };
+}
+
+function zoomInMap(value) {
+  mapObject.setZoom(value);
+}
+
+function setValues(values) {
+  selectedMarker.value = values;
+  setTimeout(() => {
+    zoomInMap(15);
+  }, 250);
+  setCenter(values.gpsPosition.latitude, values.gpsPosition.longitude);
+}
+
+function handleResize() {
+  resizeStyle.value = getResizeStyle();
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+</script>
 
 <template>
-  <div class="header">
-    <h1>Map page</h1>
+  <div class="main-container">
+    <Map :large="largeMap" :setValues="setValues" :resize="toggleMapSize" :markers="markers" :center="center"
+    @map-ready="onMapReady" />
   </div>
 </template>
 
 <style scoped>
-.header {
-  font-size: x-large;
-  color: #000;
+.main-container {
+  height: 100vh;
+  width: 100%;
 }
 </style>
