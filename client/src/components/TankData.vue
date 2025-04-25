771a4caf34de97430344c1044aec3cba7622f7ee
ItsZipzon <rune.molander@hotmail.com>
2025-04-25 14:37:23 +0200

    Added box for containing tanks

--- client/src/components/TankData.vue
+++ client/src/components/TankData.vue
@@ -24,26 +24,41 @@ onUnmounted(() => {
 });
 </script>
 <template>
-    <div class="ship-tank-data" id="ship-tank-data">
-        <svg :width="shipWidth" :height="shipHeight" xmlns="http://www.w3.org/2000/svg">
-            <!-- Top line -->
-            <line :x1="shipWidth*0.2" y1="10" :x2="shipWidth-10" y2="10" stroke="black" stroke-width="1" />
-            <!-- Right line -->
-            <line :x1="shipWidth-10" y1="9.5" :x2="shipWidth-10" :y2="shipHeight - 10 + 0.5" stroke="black" stroke-width="1" />
-            <!-- Bottom line -->
-            <line :x1="shipWidth-10" :y1="shipHeight - 10" :x2="shipWidth*0.2" :y2="shipHeight - 10" stroke="black" stroke-width="1" />
-
-            <line :x1="shipWidth*0.2 + 0.25" y1="10.25" x2="0" :y2="(shipHeight - 10)*0.5" stroke="black" stroke-width="1" />
-            <line :x1="shipWidth*0.2 + 0.25" :y1="shipHeight - 10 + 0.25" x2="0" :y2="(shipHeight - 10)*0.5" stroke="black" stroke-width="1" />
-        </svg>
+  <div class="ship-tank-data" id="ship-tank-data">
+    <div class="ship-tanks">
+      
     </div>
+    <svg :width="shipWidth" :height="shipHeight" xmlns="http://www.w3.org/2000/svg">
+      <!-- Top line -->
+      <line :x1="shipWidth * 0.2" y1="10" :x2="shipWidth - 10" y2="10" stroke="black" stroke-width="1" />
+      <!-- Right line -->
+      <line :x1="shipWidth - 10" y1="9.5" :x2="shipWidth - 10" :y2="shipHeight - 10 + 0.5" stroke="black"
+        stroke-width="1" />
+      <!-- Bottom line -->
+      <line :x1="shipWidth - 10" :y1="shipHeight - 10" :x2="shipWidth * 0.2" :y2="shipHeight - 10" stroke="black"
+        stroke-width="1" />
+
+      <line :x1="shipWidth * 0.2 + 0.25" y1="10.25" x2="0" :y2="(shipHeight - 10) * 0.5" stroke="black" stroke-width="1" />
+      <line :x1="shipWidth * 0.2 + 0.25" :y1="shipHeight - 10 + 0.25" x2="0" :y2="(shipHeight - 10) * 0.5" stroke="black"
+        stroke-width="1" />
+    </svg>
+  </div>
 </template>
 <style scoped>
 .ship-tank-data {
-    height: fit-content;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: fit-content;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.ship-tanks {
+  position: absolute;
+  height: 200px;
+  width: 600px;
+  background-color: #f0f0f0;
+  left: 0;
+  top: 0;
 }
 </style>
