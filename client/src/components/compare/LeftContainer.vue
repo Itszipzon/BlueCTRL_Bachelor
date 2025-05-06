23d7a0514b882e4c5af8d72985aa1e302fbd07dd
Eirik <eirik.imrik@gmail.com>
2025-05-06 13:29:08 +0200

    added checkmarks for better UX

--- client/src/components/compare/LeftContainer.vue
+++ client/src/components/compare/LeftContainer.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 const props = defineProps({
   boats: Array,
   selectedBoats: Array,
@@ -10,7 +9,6 @@ const emit = defineEmits(["boat-selected"]);
 const handleBoatClick = (boat) => {
   emit("boat-selected", boat);
 };
-
 </script>
 <template>
   <div class="left-container-component">
@@ -18,17 +16,27 @@ const handleBoatClick = (boat) => {
       <h3>Filter boats</h3>
       <div class="boat-list">
         <div
-          :class="['boat-item', { selected: selectedBoats.some((b) => b.id === boat.id) }]"
+          :class="[
+            'boat-item',
+            { selected: selectedBoats.some((b) => b.id === boat.id) },
+          ]"
           v-for="boat in boats"
           :key="boat.id"
           @click="handleBoatClick(boat)"
         >
-          <img
-            class="boat-flag"
-            :src="`https://flagcdn.com/h40/${boat.countryCode.toLowerCase()}.png`"
-            alt="flag"
-          />
-          <span class="boat-name">{{ boat.vesselName }}</span>
+          <div class="left-div">
+            <img
+              class="boat-flag"
+              :src="`https://flagcdn.com/h40/${boat.countryCode.toLowerCase()}.png`"
+              alt="flag"
+            />
+            <span class="boat-name">{{ boat.vesselName }}</span>
+          </div>
+          <div class="right-div">
+            <div class="checkbox">
+              <span v-if="selectedBoats.some((b) => b.id === boat.id)">✔</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -68,12 +76,19 @@ const handleBoatClick = (boat) => {
 
 .boat-item {
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  gap: 10px;
   padding: 10px;
   border: 1px solid #ddd;
   border-radius: 6px;
   background-color: #f9f9f9;
+}
+
+.left-div {
+  display: flex;  
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 }
 
 .boat-item.selected {
@@ -95,6 +110,17 @@ const handleBoatClick = (boat) => {
   font-weight: 500;
 }
 
-
-
+.checkbox {
+  width: 18px;
+  height: 18px;
+  border: 2px solid #555;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  font-weight: bold;
+  color: #005380;
+  background-color: white;
+}
 </style>
